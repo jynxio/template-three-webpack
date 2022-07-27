978419b692f9e91c58b9b6ab5d1c3c6d8ae90898
Matrix <1337816495@qq.com>
2022-07-27 21:26:03 +0800

    create template

---
+++ source/index.js
@@ -0,0 +1,55 @@
+import "/style/reset.css";
+
+import "/style/index.css";
+
+import * as three from "three";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/* ------------------------------------------------------------------------------------------------------ */
+/* Renderer */
+const renderer = new three.WebGLRenderer( { antialias: globalThis.devicePixelRatio < 2 } );
+
+renderer.setPixelRatio( Math.min( globalThis.devicePixelRatio, 2 ) );
+renderer.setSize( globalThis.innerWidth, globalThis.innerHeight );
+
+document.body.append( renderer.domElement );
+
+/* Scene */
+const scene = new three.Scene();
+
+/* Camera */
+const camera = new three.PerspectiveCamera(
+    75,
+    globalThis.innerWidth / globalThis.innerHeight,
+    0.01,
+    100,
+);
+
+scene.add( camera );
+
+/* Controls */
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.enableDamping = true;
+controls.target = new three.Vector3( 0, 0, 0.01 );
+
+/* Resize */
+globalThis.addEventListener( "resize", _ => {
+
+    renderer.setPixelRatio( Math.min( globalThis.devicePixelRatio, 2 ) );
+    renderer.setSize( globalThis.innerWidth, globalThis.innerHeight);
+
+    camera.aspect = globalThis.innerWidth / globalThis.innerHeight;
+    camera.updateProjectionMatrix();
+
+} );
+
+/* Render */
+renderer.setAnimationLoop( function loop() {
+
+    controls.update();
+
+    renderer.render( scene, camera );
+
+} );
